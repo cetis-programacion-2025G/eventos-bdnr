17017d1b6504a23cb3e7c9700c2dffc51a8f40c8
juan <blancartejuanm672@gmail.com>
2026-06-09 23:57:05 -0600

    agrego sistema de eventos

--- pantallas/eventos/agregarEvento.js
+++ pantallas/eventos/agregarEvento.js
@@ -1,17 +1,30 @@
 const { insertarEvento }                   = require('../../db/eventos/insertarEvento');
-const { preguntar }                        = require('../../utils/input');
+const { preguntar, esperarEnter }                        = require('../../utils/input');
 const { titulo: tituloUI, limpiarPantalla } = require('../../utils/ui');
 
-async function agregarEvento(datos) {
+async function agregarEvento(db) {
     limpiarPantalla();
     console.log('');
     tituloUI('AGREGAR EVENTO', 100);
+    console.log('(o en cualquier campo para cancelar)');
     const nombre    = await preguntar('  Nombre');
+    if(nombre === '0') { console.log('\n Cancelado.'); return; }
     const fecha     = await preguntar('  Fecha (YYYY-MM-DD)');
+    if(fecha === '0') { console.log('\n Cancelado.'); return;}
     const lugar     = await preguntar('  Lugar');
-    const capacidad = parseInt(await preguntar('  Capacidad'), 10);
-    const nuevo     = await insertarEvento(datos, { nombre, fecha, lugar, capacidad });
-    console.log(`\n  Evento "${nuevo.nombre}" agregado con ID ${nuevo.id}.`);
+    if(lugar === '0') { console.log('\n Cancelado.'); return;}
+    const capacidadInput = await preguntar('  Capacidad');
+    if (capacidadInput === '0') { console.log('\n Cancelado.');  return;}
+    const capacidad = parseInt(capacidadInput, 10);
+    const nuevo   = await insertarEvento(db, { 
+        nombre, 
+        fecha,
+        lugar,
+        capacidad, 
+        boletos_vendidos: 0
+     });
+    console.log(`\n  Evento "${nombre}" fue agregado `);
+    await esperarEnter();
 }
 
 module.exports = { agregarEvento };
--- pantallas/eventos/menuEventos.js
+++ pantallas/eventos/menuEventos.js
@@ -11,6 +11,8 @@ async function menuEventos(db) {
         limpiarPantalla();
         console.log('');
         titulo('EVENTOS', 100);
+        await listarEventos(db);
+        console.log('');
         console.log('─'.repeat(102));
         console.log('  1. Ver eventos');
         console.log('  2. Agregar evento');
@@ -20,10 +22,10 @@ async function menuEventos(db) {
         console.log('─'.repeat(102));
         const op = await pedirEntero('Opcion', [0, 1, 2, 3, 4]);
         switch (op) {
-            case 1: await listarEventos(db);  await esperarEnter(); break;
-            case 2: await agregarEvento(db);  await esperarEnter(); break;
-            case 3: await editarEvento(db);   await esperarEnter(); break;
-            case 4: await eliminarEvento(db); await esperarEnter(); break;
+            case 1: await listarEventos(db);   break;
+            case 2: await agregarEvento(db);   break;
+            case 3: await editarEvento(db);    break;
+            case 4: await eliminarEvento(db);  break;
             case 0: salir = true; break;
         }
     }
